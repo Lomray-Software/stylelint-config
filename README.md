d7a8c8d0ca0e785058cdeabb22eb49afa84da850
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2022-07-08 20:00:56 +0200

    feat: init

---
+++ README.md
@@ -0,0 +1,13 @@
+# stylelint-config
+
+This package provides Lomray base stylelint config as an extensible shared config.
+
+## Usage
+
+1. Install package:
+
+```sh
+  npm i --save-dev @lomray/stylelint-config
+```
+
+2. Add `extends: ["@lomray/stylelint-config"]` to your .stylelintrc.js
---
+++ index.js
@@ -0,0 +1,40 @@
+module.exports = {
+	extends: 'stylelint-config-recommended-scss',
+	plugins: [
+		'stylelint-order',
+	],
+	rules: {
+		'no-missing-end-of-source-newline': true,
+		'order/properties-alphabetical-order': true,
+		'selector-class-pattern': ['^[a-z][a-zA-Z0-9]+$', {
+			message: 'Expected name to be camelCase',
+		}],
+		'scss/at-mixin-pattern': [
+			'^(-?[a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+			{
+				message: 'Expected mixin name to be kebab-case',
+			},
+		],
+		'scss/at-function-pattern': [
+			'^(-?[a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+			{
+				message: 'Expected function name to be kebab-case',
+			},
+		],
+		'scss/percent-placeholder-pattern': [
+			'^(-?[a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+			{
+				message: 'Expected placeholder to be kebab-case',
+			},
+		],
+		'rule-empty-line-before': ['always', {
+			ignore: ['after-comment', 'first-nested', 'inside-block'],
+		}],
+		'selector-pseudo-class-no-unknown': [
+			true,
+			{
+				ignorePseudoClasses: ['global'],
+			}
+		],
+	},
+}
